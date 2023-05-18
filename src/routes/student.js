00560e595efa22d6263d46f991cb1d06340c15c2
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-18 18:00:49 -0500

    added put and delete routes

--- src/routes/student.js
+++ src/routes/student.js
@@ -24,4 +24,17 @@ router.post('/student', async (req, res, next) => {
   res.status(200).send(newStudent);
 });
 
+router.put('/student/:id', async (req, res, next) => {
+  let updatedStudent = await studentModel.update(req.body, {where: {id: req.params.id}});
+  let singleStudent = await studentModel.findAll({where: {id: req.params.id}});
+  res.status(200).send(singleStudent);
+  res.status(200).send(updatedStudent);
+});
+
+router.delete('/student/:id', async (req, res, next) => {
+  let deletedStudent = await studentModel.destroy({where: {id: req.params.id}});
+  res.status(200).send(deletedStudent);
+});
+
+
 module.exports = router;
